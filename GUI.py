df72da83e9cb029c51aee4d59da93ede482ab446
Jawahar <Jtunuguntla@gmail.com>
2020-07-18 10:20:11 +0100

    Commit Chatbot program

---
+++ GUI.py
@@ -0,0 +1,241 @@
+from tkinter import *
+from gtts import gTTS
+import datetime
+import calendar
+import random
+import wikipedia
+import warnings
+import sys
+import subprocess
+
+
+def makeGreeting():
+    '''Greet User.
+
+    Parameters
+    __________
+    No Parameters required
+
+    Return
+    ______
+    Randomly select a greeting to User.'''
+    
+    word_selected = random.choice(wakeUpWordsList)
+    return word_selected[0].upper()+word_selected[1:]
+
+
+def getPersonName(userText):
+    '''Return name of person to search from user textfield.
+
+    Parameters
+    __________
+    userText: str, not optional
+        User entered text
+
+    Return
+    ______
+    Person to search in Wikipedia.'''
+    
+    textList = userText.split("who is")
+    if len(textList) == 0:
+        return "Please Try Again."
+    return "".join(textList)
+
+
+def getDate():
+    '''return current date.
+    
+    Parameters
+    __________
+    No Parameters required
+
+    Return
+    ______
+    Date of the runtime.'''
+    
+    now = datetime.datetime.now()
+    year,month,day = str(now.year), now.month - 1, now.day - 1
+
+    date = datetime.datetime.today()
+    weekday = calendar.day_name[date.weekday()]
+
+    monthNames = ["January", "February", "March", "April", "May", "June",
+                  "July", "August", "September", "October","November",
+                  "December"]
+    
+    ordinalNumbers = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th",
+                      "8th", "9th", "10th", "11th", "12th", "13th", "14th",
+                      "15th", "16th", "17th", "18th", "19th", "20th", "21st",
+                      "22nd", "23rd", "24th", "25th", "26th", "27th", "28th",
+                      "29th", "30th", "31st"]
+
+    return "Today is "+ weekday + " " + ordinalNumbers[day] + " of " + monthNames[month] + " " + str(year)
+
+
+def getTime():
+    '''Return current time.
+
+    Parameters
+    _________
+    No Parameters required
+
+    Return
+    ______
+    Formatted string: It is " 00:00(a.m or p.m)'''
+    
+    now = datetime.datetime.now()
+    meridiem = ""
+    if now.hour < 12:
+        meridiem = "a.m"
+    else:
+        meridiem = "p.m"
+        currentHour = str(now.hour - 12)
+
+    if now.minute > 10:
+        currentMinute = str(now.minute)
+    else:
+        currentMinute = "0"+str(now.minute)
+        
+    return "It is " + currentHour + ":" + currentMinute + meridiem
+
+
+def getVoiceResponse(userText):
+    '''Return Spoken Message to User.
+
+    Parameters
+    __________
+    userText: str, not optional
+        User entered text
+
+    Return:
+    Spoken response to user.'''
+    
+    tts = gTTS(text=userText, lang="en", slow=False)
+    tts.save("assistant_response.mp3")
+    subprocess.call(["afplay", "assistant_response.mp3"])
+
+
+def addText(userText, addOn):
+    '''Return user and computer response onto chatbox.
+
+    Parameters
+    __________
+    userText: str, not optional
+        User entered text
+    addOn: str, not optional
+        User question or Computer response to add to main chatbox
+
+    Return
+    ______
+    New updated chat window.'''
+    
+    final_text = chatWindow.get("1.0", "end-1c")+"\n"+addOn+userText
+    chatWindow.delete(1.0,"end")
+    chatWindow.insert(1.0, final_text)
+
+
+def send(userText, isUser):
+    '''Return updated conversation on main chat.
+
+    Parameters
+    __________
+    userText: str, not optional
+        User entered text
+    isUser: bool, not optional
+        True: User, False: Computer
+    
+    Return
+    ______
+    Clear user entered entrybox and update user or comp tag on main chatbox.'''
+    
+    if isUser:
+        addOn = "You: "
+    else:
+        addOn = "Comp: "
+    userTextField.set("")
+    addText(userText, addOn)
+    
+
+def enterPressed(event=None):
+    '''Return user's processed request.
+
+    Parameters
+    __________
+    event: <Return>, optional
+        user either pressed enter or the dedicated send button
+
+    Return
+    ______
+    User's processed request.'''
+    
+    userText = userTextField.get()
+    send(userText, True)
+    compText = ""
+    
+    userText = userText.lower()
+    if userText in wakeUpWordsList:
+        compText = makeGreeting()
+        send(compText, False)
+        getVoiceResponse(compText)
+    if "bye" in userText:
+        getVoiceResponse("Bye")
+        root.destroy()
+        sys.exit()
+    if "date" in userText:
+        compText = getDate()
+        send(compText, False)
+        getVoiceResponse(compText)
+    if "time" in userText:
+        compText = getTime()
+        send(compText, False)
+        getVoiceResponse(compText)
+    if "who is" in userText:
+        #Check for wikipedia.exceptions.DisambiguationError
+        try:
+            compText = wikipedia.summary(getPersonName(userText), sentences=2)
+            send(compText, False)
+            getVoiceResponse(compText)
+        except wikipedia.exceptions.DisambiguationError:
+            send("I do not know this person. Please be more precise.", False)
+            getVoiceResponse("I do not know this person. Please be more precise.")
+    elif compText == "":
+        #"I do not understand" error checks
+        send("I do not understand. Please try again.", False)
+        getVoiceResponse("I do not understand. Please try again.")
+
+
+def buildGUI():
+    '''Build the main Chatbox GUI.
+
+    Parameters
+    __________
+    No parameters required but global variables are defined
+
+    Return
+    ______
+    The main GUI.'''
+    
+    global root, userTextField, chatWindow, wakeUpWordsList
+    
+    root = Tk()
+    root.title("Chatbot"), root.geometry("300x300"), root.resizable(False, False)
+
+    userTextField = StringVar(None)
+    userTextEntry = Entry(root, textvariable=userTextField, width=25)
+    userTextEntry.place(x=0,y=272)
+
+    chatWindow = Text(root, bd=1, background="yellow")
+    chatWindow.place(x=0,y=0,width=298,height=273)
+    chatWindow.insert(1.0,"Hello I am your assistant.\nHow can I help you?")
+
+    sendButton = Button(root, text="Send", padx=12, pady=3.4, command=enterPressed)
+    sendButton.place(x=236,y=273)
+    userTextEntry.bind('<Return>',enterPressed)
+
+    wakeUpWordsList = ("hey", "hi there", "hello", "greetings", "salutations", "hi", "yo")
+    root.mainloop()
+
+#Start program
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    buildGUI()
